d1dcbb776c0ab0101a54fceefc25386bb2e40935
TonyBarretor <tony.barretor@gmail.com>
2023-10-16 21:25:16 -0700

    bettycorrections

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(){
+/*
+ *main - printf prints a specific text
+ *Return: 0
+ */
+
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
