2f7f90d9ec56a9102e94e7573788e8aa92b1e59a
TonyBarretor <tony.barretor@gmail.com>
2023-10-22 16:49:48 -0700

    putchar0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/**
+ * main - putchar prints lettter by letter
+ *Return: what it mention in main.h
+ */
+
+#include "main.h"
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
