52d48719d2e57e190c66fbf4eb369b4e9212dc57
TonyBarretor <tony.barretor@gmail.com>
2023-10-22 22:18:10 -0700

    jhgkajdfhadf

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,26 +9,25 @@
 
 void print_triangle(int size)
 {
-	        if (size <= 0)
-			                {
-						                _putchar('\n');
-								                }
-		        else
-				                {
-							                int i, j;
-
-									        for (i = 0; i < size; i++)
-											                {
-														                for (j = size - 1; j > i; j--)
-																	                        {
-																					                        _putchar(' ');
-																								                        }
-
-																                for (j = 0; j <= i; j++)
-																			                        {
-																							                        _putchar('#');
-																										                        }
-																		                _putchar('\n');
-																				                }
-										                }
+	if (size <= 0)
+	{
+	 _putchar('\n');
+	}
+	else
+	{
+	int i, j;
+	
+	for (i = 0; i < size; i++)
+	{
+	for (j = size - 1; j > i; j--)
+	{
+	 _putchar(' ');
+	}
+        for (j = 0; j <= i; j++)
+	{
+	 _putchar('#');
+	}
+	 _putchar('\n');
+	}
+	}
 }
