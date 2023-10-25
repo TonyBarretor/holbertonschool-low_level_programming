8a1470dd8aa1fe0826c396552e76559190430a14
TonyBarretor <tony.barretor@gmail.com>
2023-10-25 10:14:48 -0700

    jkdfladjf

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+/**
+ *main - fjghnkafdgafof 
+ *@argc: dlgfjadlfa
+ * @argv: lijgl
+ * Return: kjdflaf
+ */
+#include <stdio.h>
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	char *nameVariable = argv[0];
+
+	printf("%s\n", nameVariable);
+
+	return (0);
+}
