174443ed9fcd6b562ff77ac81a13a4f52802f574
TonyBarretor <tony.barretor@gmail.com>
2023-10-25 15:12:21 -0700

    upt task 2 - modify the spaces

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,10 +1,16 @@
+/**
+ * main - receive 2 parameters, argc and argv
+ * @argc: counts the parameters
+ * @argv: counts the parameters
+ * Return: all the parameters typed in the input
+ */
+
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
 	int counter;
 	(void) argc;
-	
 	for (counter = 0; counter < argc; counter++)
 	{
 
