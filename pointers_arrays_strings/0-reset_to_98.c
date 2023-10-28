24b39ef345f8a23038e30f838c4a3be6c655633a
TonyBarretor <tony.barretor@gmail.com>
2023-10-27 23:09:33 -0700

    fix: betty description and trailing space

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
 /**
- * reset_t0 -
+ * reset_to_98 - change the number to 98
  * @n: the number to be changed
  * Description: takes a pointer to an int as a parameter
- * Return:
+ * Return: 0
  */
 
 #include "main.h"
 
 void reset_to_98(int *n)
-{
-	*n = 98;
+{	*n = 98;
 }
-	
