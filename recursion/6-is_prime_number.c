8553d76f39bd99c3fbdfab7e81a45b348e4ac690
TonyBarretor <tony.barretor@gmail.com>
2023-10-29 21:13:29 -0700

    fix: betty docs in task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,19 +1,32 @@
+/**
+ * check_divisibility - check for
+ * is_prime_number - prime number
+ * @n: number
+ * @i: iterations
+ * Return: 0
+ */
+
 #include "main.h"
 
-int check_divisibility(int n, int i) {
-    if (i * i > n) {
-        return 1;
-    }
-    if (n % i == 0) {
-        return 0;
-    }
-    return check_divisibility(n, i + 1);
+int check_divisibility(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (1);
+	}
+
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (check_divisibility(n, i + 1));
 }
 
-int is_prime_number(int n) {
-    if (n <= 1) {
-        return 0;
-    }
-    return check_divisibility(n, 2);
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (check_divisibility(n, 2));
 }
-
