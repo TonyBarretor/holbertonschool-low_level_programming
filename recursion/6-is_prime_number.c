0844921e148687c5324042e7169b9fa31e2c751c
TonyBarretor <tony.barretor@gmail.com>
2023-10-29 21:02:12 -0700

    fix: task 6 loops are not allowed

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,24 +1,19 @@
-/**
- * is_prime_number - returns 1 
- * @n: the number
- * Return: 1 if prime if not 0
- */
-
 #include "main.h"
 
-int is_prime_number(int n)
-{
-	int i;
+int check_divisibility(int n, int i) {
+    if (i * i > n) {
+        return 1;
+    }
+    if (n % i == 0) {
+        return 0;
+    }
+    return check_divisibility(n, i + 1);
+}
+
+int is_prime_number(int n) {
     if (n <= 1) {
         return 0;
     }
-
-    for (i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return 0;
-        }
-    }
-
-    return 1;
+    return check_divisibility(n, 2);
 }
 
