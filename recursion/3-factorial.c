77ad21e1c9408cbd90d6451a51c5c7bc5efece2f
TonyBarretor <tony.barretor@gmail.com>
2023-10-29 21:40:07 -0700

    fix: docs betty doc from tas 0 to 6

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -5,20 +5,21 @@
  * Return: 0
  */
 
-
 #include "main.h"
 #include <stdio.h>
 
 int factorial(int n)
 {
-        if (n < 0)
-
-                return (-1);
-
-        else if (n == 0)
-
-                return (1);
-
-        else
-                return (n * factorial(n - 1));
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
