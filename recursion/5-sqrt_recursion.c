d70927303e8b7b7b002880bd6f3f980d951c29bf
TonyBarretor <tony.barretor@gmail.com>
2023-10-29 21:50:43 -0700

    fix: task 5 add function find sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,21 +8,6 @@
 
 #include "main.h"
 
-int find_sqrt(int n, int i)
-{
-	if (i * i > n)
-	{
-		return (-1);
-	} else if (i * i == n)
-	{
-		return (i);
-	}
-	else
-	{
-		return (find_sqrt(n, i + 1));
-	}
-}
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
