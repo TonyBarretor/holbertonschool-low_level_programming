6c3195819d22af50cfe7e984ec5ff93ddfdb5abd
TonyBarretor <tony.barretor@gmail.com>
2023-10-31 09:59:01 -0700

    fix: betty doc and style task 2 print_dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,13 +1,21 @@
+/**
+ * print_dog - prints the data inside the structure dog
+ * @d: is the pointer of the struct named dog
+ * Return: 0
+ */
+
 #include "dog.h"
 #include <stdio.h>
 
-void print_dog(struct dog *d) {
-    if (d == NULL) {
-        return; 
-    }
-
-    printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-    printf("Age: %.6f\n", d->age);
-    printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
 
