c4139f1fd400092b51a175b87533439707edf664
TonyBarretor <tony.barretor@gmail.com>
2023-11-05 08:36:04 -0800

    fix: betty doc and segementation fault

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,16 @@
+/**
+ * print_name - the functions prints the name
+ * @name: name
+ * @f: function
+ * Return:
+ */
+
 #include "function_pointers.h"
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f != NULL)
+	{
 	f(name);
+	}
 }
