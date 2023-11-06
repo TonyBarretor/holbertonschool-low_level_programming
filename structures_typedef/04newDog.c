09ee308ec95e6f01c3039ed932b2df50f01cb994
TonyBarretor <tony.barretor@gmail.com>
2023-11-05 16:57:10 -0800

    fix: compile erros

---
+++ structures_typedef/04newDog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+dog_t *new_dog(char *name, float age, char *owner) {
+    dog_t *my_dog = malloc(sizeof(dog_t));
+
+    if (my_dog == NULL) {
+        return NULL;
+    }
+
+    my_dog->name = malloc(strlen(name) + 1);
+    if (my_dog->name == NULL) {
+        free(my_dog);
+        return NULL;
+    }
+    strcpy(my_dog->name, name);
+
+    my_dog->owner = malloc(strlen(owner) + 1);
+    if (my_dog->owner == NULL) {
+        free(my_dog->name);
+        free(my_dog);
+        return NULL;
+    }
+    strcpy(my_dog->owner, owner);
+
+    my_dog->age = age;
+
+    return my_dog;
+}
